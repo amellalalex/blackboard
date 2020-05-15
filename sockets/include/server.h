8e185d6d1bf8904278f3a7701555e0f6dc36ea95
user <user@users-MacBook-Pro.local>
2020-05-15 09:47:49 -0300

    added sockets folder

---
+++ sockets/include/server.h
@@ -0,0 +1,12 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
+
+int server_start();
+
+#endif /* end of include guard: SERVER_H */
---
+++ sockets/src/server.c
@@ -0,0 +1,86 @@
+#include "server.h"
+
+static int sock;
+
+int server_start()
+{
+  // Create socket
+  if((sock = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) // socket() failed
+  {
+    perror("socket() failed");
+    return -1;
+  }
+
+  // Create placeholder for unix domain address
+  const struct sockaddr_un address = {
+    .sun_family = AF_UNIX,
+    .sun_path   = "/Users/user/Documents/Programming/C/sockets/tunnel.socket"
+  };
+  socklen_t address_len = sizeof(address);
+
+  // Unlink socket
+  if(unlink(address.sun_path) == -1) // unlink() failed
+  {
+    perror("unlink() failed");
+    return -1;
+  }
+
+  // Bind socket
+  if(bind(sock, (struct sockaddr *) &address, sizeof(address)) == -1) // bind() failed
+  {
+    perror("bind() failed");
+    return -1;
+  }
+
+  // Start listening for up to 5 incoming connections
+  if(listen(sock, 5) == -1) // listen() failed
+  {
+    perror("listen() failed");
+    return -1;
+  }
+
+  // Create placeholder for incoming connection
+  int conn;
+
+  // Accept incoming connections until none left
+  while((conn = accept(sock, (struct sockaddr *) &address, &address_len)) != -1)
+  {
+    // Create placeholder for incoming message
+    char msg[128];
+
+    // Receive message from client
+    while(read(conn, (void *) msg, 128) == -1) // read() failing
+    {
+      perror("read() failed");
+      sleep(1);
+    }
+
+    // if(read(conn, (void *) msg, 128) == -1) // read() failed
+    // {
+    //   perror("read() failed");
+    //   return -1;
+    // }
+
+    // Null terminate message
+    msg[strlen(msg)] = '\0';
+
+    // Print message
+    printf("Incoming message from client: %s\n", msg);
+
+    // Close connection to client
+    close(conn);
+  }
+
+  // Check connection status
+  if(conn == -1) // connection error
+  {
+    perror("connection error");
+    return -1;
+  }
+
+  // Close socket
+  close(sock);
+
+  // done
+  return 0;
+}
