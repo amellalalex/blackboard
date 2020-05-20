2f589797a9235daef20daeea07ffcc370e59318a
Alex <git@alexamellal.com>
2020-05-20 10:23:53 -0300

    removed extra space

--- sockets/3. Crowd Control/src/ipc.c
+++ sockets/3. Crowd Control/src/ipc.c
@@ -23,7 +23,7 @@
 static client_t clients[MAX_NUM_CLI];
 
 // Placeholder for client handlers
-static pthread_t  client_handlers[MAX_NUM_CLI];
+static pthread_t client_handlers[MAX_NUM_CLI];
 
 int main()
 {
