dc526a4ec18dd7a9deb877a16f39de8dbfbecdc2
Alex <git@alexamellal.com>
2020-05-20 02:32:12 -0300

    added settings header

---
+++ sockets/3. Crowd Control/include/settings.h
@@ -0,0 +1,16 @@
+/*
+* settings.h
+*
+*   purpose: control default values used in program
+*   author: alex amellal
+*
+*/
+
+#ifndef SOCKETS_CROWDCONTROL_INCLUDE_SETTINGS_H
+#define SOCKETS_CROWDCONTROL_INCLUDE_SETTINGS_H
+
+// IPC
+#define MAX_NUM_CLI 5
+#define MAX_MSG_LEN 128
+
+#endif /* end of include guard: SOCKETS_CROWDCONTROL_INCLUDE_SETTINGS_H */
