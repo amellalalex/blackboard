85d7c58fc9250211ee8af0f2bfe720bf0070b274
Alex <git@alexamellal.com>
2020-05-25 06:40:12 -0300

    completed simple test_handle_client_requests

--- sockets/3. Crowd Control/src/test.c
+++ sockets/3. Crowd Control/src/test.c
@@ -1,3 +1,6 @@
+// Unit testing signature
+#define UNIT_TESTING 1
+
 // Project source
 #include "client_handler.c"
 
@@ -29,7 +32,7 @@ static void test_get_free_client()
 
   // Assign clients pointer to test clients array
   clients = test_clients;
-  // set_clients(test_clients);
+
 
   // Test get_free_client
   assert_true(get_free_client() == expected_index);
@@ -39,8 +42,8 @@ static void test_get_free_client()
 int __wrap_read(int sock, void * buf, int buf_len)
 {
   // Assert parameter values
-  assert_int_equal(sock, (int) mock());               // sock parameter
-  assert_non_null(buf);                    // buf parameter
+  assert_int_equal(sock, (int) mock());     // sock parameter
+  assert_non_null(buf);                     // buf parameter
   assert_int_equal(buf_len, (int) mock());  // buf_len parameter
 
   // Send mocked request
@@ -53,6 +56,15 @@ int __wrap_read(int sock, void * buf, int buf_len)
 // Mocked write function
 int __wrap_write(int sock, void * buf, int buf_len)
 {
+  // Assert parameter values
+  assert_int_equal(sock, (int) mock());     // sock parameter
+  assert_non_null(buf);                     // buf parameter
+  assert_int_equal(buf_len, (int) mock());  // buf_len parameter
+
+  // Check mocked message
+  int result = strcmp((char *) buf, (char *) mock());
+  assert_int_equal(result, 0);
+
   return (int) mock();
 }
 
@@ -60,7 +72,8 @@ int __wrap_write(int sock, void * buf, int buf_len)
 static void test_handle_client_requests()
 {
   // Set designated client index for testing
-  int expected_index = 2;
+  int expected_src_index  = 2;
+  int expected_dest_index = 4;
 
   // Init mocked clients array
   client_t test_clients[MAX_NUM_CLI];
@@ -77,29 +90,39 @@ static void test_handle_client_requests()
 
   // Assign clients pointer to test clients array
   clients = test_clients;
-  // set_clients(test_clients);
 
-  // Create placeholder for mocked client name
-  char name[3];
-  sprintf(name, "%.3d", expected_index);
+  // Create placeholder for mocked client names
+  char test_src_name[3], test_dest_name[3];
+  sprintf(test_src_name, "%.3d", expected_src_index);
+  sprintf(test_dest_name, "%.3d", expected_dest_index);
 
   // Init mocked client
-  client_t test_client = test_clients[expected_index];
+  client_t test_client = test_clients[expected_src_index];
 
   // Create placeholder for mocked request
-  char * test_req = "004 hello friend";
+  char test_req[MAX_MSG_LEN];
+  sprintf(test_req, "%.3s hello friend", test_dest_name);
+
+  // Create placeholder for mocked message
+  char test_message[MAX_MSG_LEN];
+  sprintf(test_message, "%.3s hello friend", test_src_name);
 
   // Setup expected values for mock read()
-  will_return(__wrap_read, test_clients[expected_index].conn);  // sock parameter
-  will_return(__wrap_read, MAX_MSG_LEN);                        // buf_len parameter
-  will_return(__wrap_read, test_req);                           // mocked request
-  will_return(__wrap_read, strlen(test_req));                   // return value
+  will_return(__wrap_read, test_clients[expected_src_index].conn);  // sock parameter
+  will_return(__wrap_read, MAX_MSG_LEN);                            // buf_len parameter
+  will_return(__wrap_read, test_req);                               // mocked request
+  will_return(__wrap_read, strlen(test_req));                       // return value
 
   // Setup expected values for mock write()
-  will_return(__wrap_write, -1);
+  will_return(__wrap_write, test_clients[expected_dest_index].conn); // sock parameter
+  will_return(__wrap_write, strlen(test_req));                      // buf_len parameter
+  will_return(__wrap_write, test_message);                          // mocked message
+  will_return(__wrap_write, strlen(test_message));                  // return value
 
   // Run function
   handle_client_requests(&test_client);
+
+  
 
 }
 
