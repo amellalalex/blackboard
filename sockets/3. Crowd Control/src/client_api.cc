2c26bfa2e2ad9e8fcf162378b7b01457452e1555
Alex <git@alexamellal.com>
2020-05-28 17:26:27 -0300

    added IPC client api

---
+++ sockets/3. Crowd Control/src/client_api.cc
@@ -0,0 +1,10 @@
+/*
+* client_api.cc
+*
+*   purpose: provides API for other subsystems to use the IPC system as clients.
+*   author: alex amellal
+*
+*/
+
+// Project headers
+#include "ipc/client_api.h"
